8fff58b04eeee0c99a5527dd3a74809a4da6083a
SamaelDev <111207396+JonathanSamael@users.noreply.github.com>
2025-10-13 14:20:33 -0300

    "fix: github guardian security"

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:open_banking_app/core/api_client.dart';
 import 'package:open_banking_app/repository/user_repository.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -13,9 +14,9 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     //TODO - Teste com token mockado - Funcionamento OK, mudar resultado do client.getAll para list.
-    String token =
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ikt0aFlyc09tWFdhYzdmWlRibFZ2bCIsImVtYWlsIjoiZW1haWxAdGVzdC5jb20iLCJpYXQiOjE3NjAzNjk3MTcsImV4cCI6MTc2MDM3MzMxN30.NNlHTlvLIEux9RC-0sHniCiaHldtXglxGMtZwZW5zz8";
-
+   final tokenEnv = dotenv.env['TOKEN']!;
+    String token = 'Bearer $tokenEnv';
+    
     return MaterialApp(
       home: Scaffold(
         body: Column(
